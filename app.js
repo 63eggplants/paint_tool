31f27ad74670787a4d676fda96aa0783122ae8b7
이하영 <2hayoung@ihayeong-ui-MacBook-Air.local>
2021-07-14 16:45:49 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+/*set context default*/
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
-function stopPainting() {
-  painting = false;
-}
 function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
+  if (!painting) {
+    return;
+  } else {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseDown(event) {
+function startPainting(event) {
   painting = true;
   const x = event.offsetX;
   const y = event.offsetY;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
 }
-function onMouseUp(event) {
-  stopPainting();
+function stopPainting() {
+  ctx.closePath();
+  painting = false;
 }
 
 if (canvas) {
-  canvas.addEventListener('mousedown', onMouseDown);
+  canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
