c7896b8a384485e0ead0be424d6cd50dfb53a2c5
이하영 <2hayoung@ihayeong-ui-MacBook-Air.local>
2021-07-14 17:14:26 +0900

    Brush Size

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const canvas = document.getElementById('jsCanvas');
+const canvas = document.querySelector('.jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll('.jsColor');
+const range = document.querySelector('.jsRange');
+const mode = document.querySelector('.jsMode');
 
 /*set context default*/
 ctx.strokeStyle = '#2c2c2c';
-ctx.lineWidth = 2.5;
+ctx.lineWidth = range.value;
 
 let painting = false;
+let filling = false;
 
 function onMouseMove(event) {
   if (!painting) {
@@ -30,9 +34,41 @@ function stopPainting() {
   painting = false;
 }
 
+function changeColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function changeBrushSize(event) {
+  const brushSize = event.target.value;
+  ctx.lineWidth = brushSize;
+}
+
+function changeMode(event) {
+  if (filling) {
+    filling = false;
+    mode.innerText = 'fill';
+  } else {
+    filling = true;
+    mode.innerText = 'paint';
+  }
+}
 if (canvas) {
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
+}
+
+if (colors) {
+  colors.forEach((color) => color.addEventListener('click', changeColor));
+}
+
+if (range) {
+  range.addEventListener('input', changeBrushSize);
+}
+
+if (mode) {
+  console.log(mode);
+  mode.addEventListener('click', changeMode);
 }
